42be0027e73ef3c5a2f243267242916b9218bd31
Maxwell Haley <maxwell.haley@openmailbox.org>
2016-11-22 23:00:52 -0500

    Added constructor to build the binary tree.

--- src/morseCodeTranslator/MorseCodeTree.java
+++ src/morseCodeTranslator/MorseCodeTree.java
@@ -1,13 +1,70 @@
 package morseCodeTranslator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import adts.binarytree.BinaryTree;
+import adts.binarytree.BinaryTreeNode;
+import adts.binarytree.LinkedBinaryTree;
+import adts.binarytree.Visitor;
 
 public class MorseCodeTree
 {
-	BinaryTree<String> morseTree;
-	String[] morseArray;
+	private BinaryTree<String> morseTree;
+	private Visitor<String> visitor;
 
-	protected String encode()
+	MorseCodeTree() throws FileNotFoundException
+	{
+		// char aSize = 'a';
+		// int size = (int) aSize;
+		String englishChar = null;
+		String morseChar = null;
+		String[] temp;
+		File file = new File("./morseCodeTable.txt");
+		Scanner input = new Scanner(file);
+		BinaryTreeNode<String> lastNode = null;
+		visitor = new CodeVisitor<String>();
+		morseTree = new LinkedBinaryTree<String>(" ");
+
+		while (input.hasNextLine())
+		{
+			temp = input.nextLine().split(" ");
+			englishChar = temp[0];
+			morseChar = temp[1];
+			lastNode = morseTree.root();
+
+			for (int k = 0; k < morseChar.length(); k++)
+			{
+				if (morseChar.length() - 1 == k)
+				{
+					if (morseChar.charAt(k) == '.')
+					{
+						morseTree.makeLeftChild(lastNode, englishChar);
+					}
+					else
+					{
+						morseTree.makeRightChild(lastNode, englishChar);
+					}
+				}
+				else
+				{
+					if (morseChar.charAt(k) == '.')
+					{
+						lastNode = lastNode.leftChild();
+					}
+					else
+					{
+						lastNode = lastNode.rightChild();
+					}
+				}
+			}
+		}
+
+		input.close();
+	}
+
+	protected String encode(String toEncode)
 	{
 		// TODO
 		/*
@@ -15,6 +72,12 @@ public class MorseCodeTree
 		 * all into an String that is then shown to the user.
 		 */
 
+		morseTree.levelOrderTraversal(visitor);
+		String[] fuck = ((CodeVisitor<String>) visitor).getMorseArray();
+		for (int j = 0; j < fuck.length; j++)
+		{
+			System.out.println(fuck[j]);
+		}
 		return null;
 	} // encode()
 
