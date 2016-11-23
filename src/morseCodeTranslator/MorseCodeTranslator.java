09acbc32fac8dc9e926453609c25fa94d3f8ba98
Maxwell Haley <maxwell.haley@openmailbox.org>
2016-11-22 23:00:40 -0500

    Added basic functionality for testing.

--- src/morseCodeTranslator/MorseCodeTranslator.java
+++ src/morseCodeTranslator/MorseCodeTranslator.java
@@ -1,10 +1,22 @@
 package morseCodeTranslator;
 
-public class MorseCodeTranslator {
+import java.io.FileNotFoundException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class MorseCodeTranslator
+{
 
+	public static void main(String[] args)
+	{
+		try
+		{
+			MorseCodeTree test = new MorseCodeTree();
+			test.encode("test");
+		}
+		catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
