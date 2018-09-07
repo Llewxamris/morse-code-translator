28e968c5c90cb7552917254824f6e0dd784ebd63
Maxwell R. Haley <maxwell.r.haley@gmail.com>
2018-09-06 21:57:02 -0400

    Remove tests for features no longer in development

--- src/test/java/org/maxwellhaley/mct/EncodeTests.java
+++ src/test/java/org/maxwellhaley/mct/EncodeTests.java
@@ -37,32 +37,4 @@ class EncodeTests {
     String outputMorse = "... .--. .... -.-- -. -..-  --- ..-.  -... .-.. .- -.-. -.-  --.- ..- .- .-. - --..  .--- ..- -.. --. .  -- -.--  ...- --- .--";
     assertEquals(outputMorse, mct.encode(inputAlpha));
   }
-  
-//  @Test
-//  void encodeComplexSentance() throws IOException {
-//    MorseCodeTree mct = new MorseCodeTree();
-//    String inputAlpha = "Sphynx of black quartz, judge my vow!";
-//    String outputMorse = "... .--. .... -.-- -. -..-  --- ..-.  -... .-.. .- -.-. -.-  --.- ..- .- .-. - --.. --··--  .--- ..- -.. --. .  -- -.--  ...- --- .-- -·-·--";
-//    
-//    assertEquals(outputMorse, mct.encode(inputAlpha));
-//  }
-//  
-//  @Test
-//  void encodeNumerals() throws IOException {
-//    MorseCodeTree mct = new MorseCodeTree();
-//    String inputAlpha = "0123456789";
-//    String outputMorse = "----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----.";
-//    
-//    assertEquals(outputMorse, mct.encode(inputAlpha));
-//  }
-//  
-//  @Test
-//  void encodeStdSpecialChars() throws IOException {
-//    MorseCodeTree mct = new MorseCodeTree();
-//    String inputAlpha = "!@#$%^&*()-_=+,<.>/?|\\;:'\"~`";
-//    String outputMorse = "-·-·-- ·--·-· ···-··- ·-··· -·--· -·--·- -····- ··--·- -···- ·-·-· --··-- ·-·-·- -··-· ··--·· -·-·-· ---··· ·----· ·-··-·";
-//    
-//    assertEquals(outputMorse, mct.encode(inputAlpha));
-//  }
-
 }
